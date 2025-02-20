f4bb546da2f3ca67864e385b73d9997a75304094
Aadish Vikram Chakravorty <aadishchakravorty@gmail.com>
2025-02-20 12:25:42 +0530

    Commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+            <h1>Smart Task Manager</h1>
+            {/* Your components will go here */}
+        </div>
+    );
+};
+
+export default App;
